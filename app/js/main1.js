2a5fe7fae1cf725e65d9ff14127cefa353c8bbbc
Dima27011993 <73137302+Dima27011993@users.noreply.github.com>
2021-06-18 20:36:56 +0300

    work with js content to push html

--- app/js/main1.js
+++ app/js/main1.js
@@ -45,3 +45,94 @@
 // const numberSort = [5,4,3,2,1]
 // numberSort.sort((a, b) => b - a )
 // console.log(numberSort)
+const abonements = [
+    {
+        type: 'акционное',
+        name: 'Зона бикини и подмышечных впадин',
+        description: 'Лазерная эпиляция бикини и подмышечных впадин',
+        count: 5,
+        price: 10000,
+        currency: 'руб',
+        priceDesc: '2500 руб — 1 процедура' 
+    },
+    {
+        type: 'акционное',
+        name: 'Комплекс «Мальвина»',
+        description: 'Голени, тотальное бикини, подмышечные впадины, линия живота, пальчики ног',
+        count: 5,
+        price: 15990,
+        currency: 'руб',
+        priceDesc: '3990 руб — 1 процедура' 
+    },
+    {
+        type: 'акционное',
+        name: 'Комплекс «Лайт»',
+        description: 'Ноги полностью, тотальное бикини, подмышечные впадины, линия живота, пальчики ног',
+        count: 3,
+        price: 12500,
+        currency: 'руб',
+        priceDesc: '4490 руб — 1 процедура' 
+    }
+]
+
+abonements.forEach(item =>{
+$('.abonements__Block_price').append(`
+<div class="block__col">
+<div class="first__item">
+    <button class="sale_xs"><a href="#">${item.type}</a></button>
+    <p class="p16_28b">
+    ${item.name}
+    </p>
+    <p class="p14_24xs">${item.description}</p>
+
+</div>
+<div class="two__item">
+    <span>${item.count} процедур</span>
+</div>
+<div class="three__item">
+    <span>${item.price} ${item.currency}</span>
+    <p class="p14_24xs">${item.priceDesc}</p>
+</div>
+<div class="four__item">
+    <button class="button_S">
+        <a href="#">записаться</a>
+    </button>
+</div>
+</div>
+`)
+});
+
+servicesJS = [
+    {
+       discription: 'Лазерная эпиляция',
+       images: 'images/services/1.jpg'
+        
+    },
+    {
+       discription: 'Косметология',
+       images: 'images/services/2.jpg'
+    },
+    {
+       discription: 'Ногтевой сервис',
+       images: 'images/services/3.jpg'
+    }
+]
+
+servicesJS.forEach(item =>{
+    $('.wrapper__images__abonements__block').append(`
+
+    <div class="images__abonements__block">
+    <img src="${item.images}" alt="">
+    <div class="inner__images__abonements__block">
+    <img src="images/services/Vector.svg" alt="" class="vector">
+    <p class="p18_32"><a href="#"> ${item.discription}</a></p>
+</div>
+</div>
+
+`)
+})
+
+
+
+
+
